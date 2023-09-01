3d0aebdb53a3dc2353f5ad79d5f63e73e60e3038
Norman Oliver Leonor <norman.leonor@outgive.com>
2023-09-02 00:11:27 +0800

    First Commit

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "@react-native-community"
+
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        semi: ["error", "never"],
+        "reacy/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+        "no-use-before-define": [
+            "error",
+            { "functions": false, "classes": true, "variables": false}
+        ]
+    }
+}
